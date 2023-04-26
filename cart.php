a81618bc11e38ae214505c4a69ecdab0e4699682
bot <76986919+p0tatoes@users.noreply.github.com>
2023-04-26 15:51:57 +0800

    Actually fixed the issue on the previous commit lol

--- cart.php
+++ cart.php
@@ -15,12 +15,16 @@ $store_products = [
 ];
 
 $products_cart = isset($_COOKIE['products_cart']) ? unserialize($_COOKIE['products_cart']) : [];
+setcookie("products_cart", serialize($products_cart), time() + 86400, '/');
 $carted_prod = $_REQUEST['prod_id'] ?? null;
 
+/**
+ * ADDING TO CART WORKS! but has error on first click
+ */
 if ($carted_prod == 1) {
     // increase quantity of man t-shirt in the cart by 1
     $added_product = $store_products[0];
-    $prod_quantity = isset($_COOKIE['products_cart']) ? $products_cart[0]['quantity'] : 0;
+    $prod_quantity = $products_cart[0]['quantity'] ?? 0;
     $added_product['quantity'] = $prod_quantity + 1;
     $products_cart[0] = $added_product;
     setcookie("products_cart", serialize($products_cart), time() + 86400, '/');
@@ -29,7 +33,7 @@ if ($carted_prod == 1) {
 if ($carted_prod == 2) {
     // increase quantity of man polo shirt in the cart by 1
     $added_product = $store_products[1];
-    $prod_quantity = isset($_COOKIE['products_cart']) ? $products_cart[1]['quantity'] : 0;
+    $prod_quantity = $products_cart[1]['quantity'] ?? 0;
     $added_product['quantity'] = $prod_quantity + 1;
     $products_cart[1] = $added_product;
     setcookie("products_cart", serialize($products_cart), time() + 86400, '/');
@@ -38,7 +42,7 @@ if ($carted_prod == 2) {
 if ($carted_prod == 3) {
     // increase quantity of woman dress in the cart by 1
     $added_product = $store_products[2];
-    $prod_quantity = isset($_COOKIE['products_cart']) ? $products_cart[2]['quantity'] : 0;
+    $prod_quantity = $products_cart[2]['quantity'] ?? 0;
     $added_product['quantity'] = $prod_quantity + 1;
     $products_cart[2] = $added_product;
     setcookie("products_cart", serialize($products_cart), time() + 86400, '/');
