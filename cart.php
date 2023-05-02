8c1e79a0c78722d66fe7a9734f051ce89baeccb8
bot <76986919+p0tatoes@users.noreply.github.com>
2023-05-02 15:31:47 +0800

    add to cart functionality finally works and is in sir alv's specifications, probably

--- cart.php
+++ cart.php
@@ -1,52 +1,52 @@
 <?php
-$store_products = [
-    [
-        "id" => 1,
-        "name" => "man t-shirt",
-        "picture" => 'images/tshirt-img.png'
-    ],
-    [
-        "id" => 2,
-        "name" => "man polo shirt",
-        "picture" => 'images/dress-shirt-img.png'
-    ],
-    [
-        "id" => 3,
-        "name" => "woman dress",
-        "picture" => 'images/women-clothes-img.png'
-    ]
-];
+include 'head.php';
 
 $products_cart = isset($_COOKIE['products_cart']) ? unserialize($_COOKIE['products_cart']) : [];
 $carted_prod = $_REQUEST['prod_id'] ?? null;
 
 /**
- * ADDING TO CART WORKS!
+ * ? Adding to cart works, probably
+ * https://youtu.be/QwuWkKGcvVY
+ * me rn
  */
-if ($carted_prod == 1) {
-    // increase quantity of man t-shirt in the cart by 1
-    $added_product = $store_products[0];
-    $prod_quantity = $products_cart[0]['quantity'] ?? 0;
-    $added_product['quantity'] = $prod_quantity + 1;
-    $products_cart[0] = $added_product;
-    setcookie("products_cart", serialize($products_cart), time() + 86400, '/');
-}
+$product_query = "SELECT * FROM products";
+$product_search = mysqli_query($lazada, $product_query);
+$product_list = mysqli_fetch_all($product_search);
+foreach ($product_list as $key => $product) {
+    $product_id = $product[0];
+    $product_category = $product[1];
+    $product_name = $product[2];
+    $product_description = $product[3];
+    $product_image = $product[5];
+    $product_quantity = $product[6];
+    $product_price = $product[8] > 0 ? $product[8] : $product[7];
+    if ($carted_prod == $product_id) {
+        /**
+         * Checks if product id is in products_cart and gets the key for the array containing said product
+         */
+        $in_cart = false;
+        foreach ($products_cart as $key2 => $product2) {
+            if ($product_id == $product2[0]) {
+                $in_cart = true;
+                $cart_id = $key2;
+            }
+        }
 
-if ($carted_prod == 2) {
-    // increase quantity of man polo shirt in the cart by 1
-    $added_product = $store_products[1];
-    $prod_quantity = $products_cart[1]['quantity'] ?? 0;
-    $added_product['quantity'] = $prod_quantity + 1;
-    $products_cart[1] = $added_product;
-    setcookie("products_cart", serialize($products_cart), time() + 86400, '/');
-}
-
-if ($carted_prod == 3) {
-    // increase quantity of woman dress in the cart by 1
-    $added_product = $store_products[2];
-    $prod_quantity = $products_cart[2]['quantity'] ?? 0;
-    $added_product['quantity'] = $prod_quantity + 1;
-    $products_cart[2] = $added_product;
-    setcookie("products_cart", serialize($products_cart), time() + 86400, '/');
+        /**
+         * adds newly carted product to products_cart, with quantity 1
+         * 
+         * if product is already in products_cart, increments quantity by 1
+         */
+        if ($in_cart === false) {
+            $products_cart[] = [$product_id, $product_category, $product_name, $product_description, $product_image, $product_quantity, $product_price, 1];
+            echo 'in_cart == false <br>';
+        } else {
+            $carted_quantity = $products_cart[$cart_id][7];
+            $products_cart[$cart_id] = [$product_id, $product_category, $product_name, $product_description, $product_image, $product_quantity, $product_price, $carted_quantity + 1];
+            echo 'in_cart == true <br>';
+        }
+        setcookie("products_cart", serialize($products_cart), time() + 86400, '/');
+        print_r($products_cart);
+    }
 }
 ?>
