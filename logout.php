a13bf0c982e4bd320cb7ecff3e68ce59c8fdf0c9
p0tatoes <76986919+p0tatoes@users.noreply.github.com>
2023-05-04 22:08:54 +0800

    logging out now removes cookies for user's id and cart

--- logout.php
+++ logout.php
@@ -1,5 +1,7 @@
 <?php
+setcookie("user_id", "", time() - 86400, '/');
 setcookie("email", "", time() - 86400, '/');
 setcookie("type", "", time() - 86400, '/');
+setcookie("products_cart", "", time() - 86400, '/');
 echo "<meta http-equiv='refresh' content='0;url=index.php'>";
 ?>
