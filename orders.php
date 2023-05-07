6902b56e6ec3a0ef534dc9b49114a0323c9ed807
p0tatoes <76986919+p0tatoes@users.noreply.github.com>
2023-05-07 20:29:56 +0800

    added functionality to show customer's orders in the orders page, categorized by order status

--- orders.php
+++ orders.php
@@ -38,16 +38,12 @@ $user_type = $_COOKIE['type'] ?? '';
     <!-- fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
     <!-- font awesome -->
-    <link rel="stylesheet" type="text/css"
-        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--  -->
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- owl stylesheets -->
-    <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Poppins:400,700&display=swap&subset=latin-ext"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Poppins:400,700&display=swap&subset=latin-ext" rel="stylesheet">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesoeet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
-        media="screen">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
 </head>
 
 <body>
@@ -60,12 +56,12 @@ $user_type = $_COOKIE['type'] ?? '';
                     <div class="col-sm-12">
                         <div class="custom_menu">
                             <?php
-                            if (!isset($_COOKIE['email'])): ?>
+                            if (!isset($_COOKIE['email'])) : ?>
                                 <ul>
                                     <li><a href="./index.php?action=register&#register">Register</a></li>
                                     <li><a href="./index.php?action=login&#login">Log in</a></li>
                                 </ul>
-                            <?php else: ?>
+                            <?php else : ?>
                                 <ul>
                                     <li>
                                         Welcome,
@@ -112,11 +108,9 @@ $user_type = $_COOKIE['type'] ?? '';
                     <!-- ? remove probably -->
                     <!-- <a href="./index.php"><span class="toggle_icon"><img src="images/toggle-icon.png"></span></a> -->
 
-                    <a href="./index.php"><span class="toggle_icon"><i class="fa fa-home" aria-hidden="true"
-                                style="transform: scale(3.5);"></i></span></a>
+                    <a href="./index.php"><span class="toggle_icon"><i class="fa fa-home" aria-hidden="true" style="transform: scale(3.5);"></i></span></a>
                     <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Category
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Category
                         </button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <a class="dropdown-item" href="#">Action</a>
@@ -128,8 +122,7 @@ $user_type = $_COOKIE['type'] ?? '';
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Search this blog">
                             <div class="input-group-append">
-                                <button class="btn btn-secondary" type="button"
-                                    style="background-color: #f26522; border-color:#f26522 ">
+                                <button class="btn btn-secondary" type="button" style="background-color: #f26522; border-color:#f26522 ">
                                     <i class="fa fa-search"></i>
                                 </button>
                             </div>
@@ -149,8 +142,8 @@ $user_type = $_COOKIE['type'] ?? '';
                             </div>
                         </div>
                         <?php
-                        if ($user_type == 'customer'):
-                            ?>
+                        if ($user_type == 'customer') :
+                        ?>
                             <div class="login_menu">
                                 <ul>
                                     <li><a href="index.php?cart=true">
@@ -172,8 +165,7 @@ $user_type = $_COOKIE['type'] ?? '';
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <div class="row">
-                                <div class="col-sm-12"
-                                    style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+                                <div class="col-sm-12" style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
                                     <?php
                                     if ($user_type == 'admin') {
                                         include 'calendar.php';
@@ -218,8 +210,7 @@ $user_type = $_COOKIE['type'] ?? '';
 
 
     <!-- #My Orders start-->
-    <div class="fashion_section"
-        style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+    <div class="fashion_section" style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
         <!-- Links to filter orders based on status -->
         <div style="display: flex; align-items: center; justify-content: center; columns: 100px 3;">
             <?php
@@ -228,10 +219,10 @@ $user_type = $_COOKIE['type'] ?? '';
                  * Query statements for count of pending, accepted, completed, and returned/refunded orders
                  */
                 $user_id = $_COOKIE['user_id'];
-                $pending_query = "SELECT COUNT(*) AS count FROM purchases WHERE status='pending'";
-                $accepted_query = "SELECT COUNT(*) AS count FROM purchases WHERE status='accepted'";
-                $completed_query = "SELECT COUNT(*) AS count FROM purchases WHERE status='completed'";
-                $returned_refunded_query = "SELECT COUNT(*) AS count FROM purchases WHERE status='returned' OR status='refunded'";
+                $pending_query = "SELECT COUNT(*) AS count FROM purchases WHERE status='pending' AND user_id=$user_id";
+                $accepted_query = "SELECT COUNT(*) AS count FROM purchases WHERE status='accepted' AND user_id=$user_id";
+                $completed_query = "SELECT COUNT(*) AS count FROM purchases WHERE status='completed' AND user_id=$user_id";
+                $returned_refunded_query = "SELECT COUNT(*) AS count FROM purchases WHERE (status='returned' OR status='refunded') AND user_id=$user_id";
 
                 /**
                  * contains the count of pending, accepted, completed, and returned/refunded orders
@@ -240,44 +231,97 @@ $user_type = $_COOKIE['type'] ?? '';
                 $count_accepted = mysqli_fetch_assoc(mysqli_query($lazada, $accepted_query))['count'];
                 $count_completed = mysqli_fetch_assoc(mysqli_query($lazada, $completed_query))['count'];
                 $count_returned_refunded = mysqli_fetch_assoc(mysqli_query($lazada, $returned_refunded_query))['count'];
-                ?>
+            ?>
                 <a href="?status=pending">
-                    <p style="text-align: center; font-size: large; font-weight: 700;">Pending(
-                        <?php echo $count_pending ?>)
+                    <p style="text-align: center; font-size: large; font-weight: 700;">
+                        Pending(<?php echo $count_pending ?>)
                     </p>
                 </a>
                 <a href="?status=accepted">
-                    <p style="text-align: center; font-size: large; font-weight: 700;">Accepted(
-                        <?php echo $count_accepted ?>)
+                    <p style="text-align: center; font-size: large; font-weight: 700;">
+                        Accepted(<?php echo $count_accepted ?>)
                     </p>
                 </a>
                 <a href="?status=completed">
-                    <p style="text-align: center; font-size: large; font-weight: 700;">Completed(
-                        <?php echo $count_completed ?>)
+                    <p style="text-align: center; font-size: large; font-weight: 700;">
+                        Completed(<?php echo $count_completed ?>)
                     </p>
                 </a>
                 <a href="?status=return_refund">
-                    <p style="text-align: center; font-size: large; font-weight: 700;">Return/Refund(
-                        <?php echo $count_returned_refunded ?>)
+                    <p style="text-align: center; font-size: large; font-weight: 700;">
+                        Return/Refund(<?php echo $count_returned_refunded ?>)
                     </p>
                 </a>
             <?php } ?>
         </div>
 
-        <!-- List of orders, filters based on order status -->
-        <table>
+        <table style="text-align: center;">
             <thead>
                 <tr>
                     <th style="padding-left: 120px; padding-right: 120px; padding-bottom: 25px;">Image</th>
-                    <th style="padding-left: 120px; padding-right: 120px; padding-bottom: 25px;">Description</th>
                     <th style="padding-left: 120px; padding-right: 120px; padding-bottom: 25px;">Name</th>
                     <th style="padding-left: 120px; padding-right: 120px; padding-bottom: 25px;">Quantity</th>
                     <th style="padding-left: 120px; padding-right: 120px; padding-bottom: 25px;">Price</th>
-                    <th style="padding-left: 120px; padding-right: 120px; padding-bottom: 25px;">Actions</th>
+                    <th style="padding-left: 120px; padding-right: 120px; padding-bottom: 25px;">Order Date</th>
+                    <th style="padding-left: 120px; padding-right: 120px; padding-bottom: 25px;">Status</th>
                 </tr>
             </thead>
             <tbody>
-                <!-- TODO: add functionality to show orders from customers -->
+                <?php
+                if ($order_status === 'accepted') {
+                    $orders_query_statement = "SELECT prod.image_link AS image_link, prod.name AS name, prod.category AS category, pur.quantity AS quantity, pur.total_price AS price, date, status FROM purchases pur INNER JOIN products prod ON pur.product_id=prod.id WHERE user_id=$user_id AND status='accepted'";
+                } elseif ($order_status === 'completed') {
+                    $orders_query_statement = "SELECT prod.image_link AS image_link, prod.name AS name, prod.category AS category, pur.quantity AS quantity, pur.total_price AS price, date, status FROM purchases pur INNER JOIN products prod ON pur.product_id=prod.id WHERE user_id=$user_id AND status='completed'";
+                } elseif ($order_status === 'return_refund') {
+                    $orders_query_statement =  "SELECT prod.image_link AS image_link, prod.name AS name, prod.category AS category, pur.quantity AS quantity, pur.total_price AS price, date, status FROM purchases pur INNER JOIN products prod ON pur.product_id=prod.id WHERE user_id=$user_id AND (status='returned' OR status='refunded')";
+                } else {
+                    /**
+                     * pending orders is the default status shown to the user
+                     */
+                    $orders_query_statement =  "SELECT prod.image_link AS image_link, prod.name AS name, prod.category AS category, pur.quantity AS quantity, pur.total_price AS price, date, status FROM purchases pur INNER JOIN products prod ON pur.product_id=prod.id WHERE user_id=$user_id AND status='pending'";
+                }
+                /**
+                 * contains list of customer's orders based on selected status (see conditionals above)
+                 */
+                $order_list = mysqli_fetch_all(mysqli_query($lazada, $orders_query_statement), MYSQLI_ASSOC);
+
+                /**
+                 * List of ordered products by the customers, categorized by order's status
+                 */
+                foreach ($order_list as $key => $order) {
+                    $order_image = $order['image_link'];
+                    $order_name = $order['name'];
+                    $order_category = $order['category'];
+                    $order_quantity = $order['quantity'] . 'x';
+                    $total_price = $order['price'];
+                    $order_date = $order['date'];
+                    $order_status = $order['status'];
+
+                    $table_row = <<<HTML
+                    <tr>
+                        <td>
+                            <img src="$order_image" alt="$order_name">
+                        </td>
+                        <td>
+                            [$order_category] $order_name
+                        </td>
+                        <td>
+                            $order_quantity
+                        </td>
+                        <td>
+                            $total_price
+                        </td>
+                        <td>
+                            $order_date
+                        </td>
+                        <td>
+                            $order_status
+                        </td>
+                    </tr>
+                    HTML;
+                    echo $table_row;
+                }
+                ?>
             </tbody>
         </table>
     </div>
