6874f2ce1e77799bf54c5d4b76eb303ba41e7abd
bot <76986919+p0tatoes@users.noreply.github.com>
2023-05-08 13:30:56 +0800

    changed title of orders page to reflect its purpose

--- orders.php
+++ orders.php
@@ -10,7 +10,7 @@ $user_type = $_COOKIE['type'] ?? '';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eflyer</title>
+    <title>Eflyer - Orders</title>
     <!-- basic -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
