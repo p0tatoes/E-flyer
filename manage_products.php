79c2b513f10203aec296dc6c81b395be0da3b47a
bot <76986919+p0tatoes@users.noreply.github.com>
2023-05-17 13:59:14 +0800

    added: placeholder button for chat functionality

--- manage_products.php
+++ manage_products.php
@@ -119,6 +119,12 @@ if ($admin_action === "delete_product" && isset($_REQUEST["id"])) {
 </head>
 
 <body>
+    <div style="background-color: goldenrod; border-radius: 50%; width: min-content; height: min-content; padding: 5px 15px; position: fixed; box-shadow: 3px 3px 10px #262626; bottom: 20px; right: 20px;">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" style="width: 50px; height: 50px; color: whitesmoke;">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+        </svg>
+    </div>
+
     <!-- show modal if editing a product. send request containing id to be deleted if deleting a product -->
     <?php
     if ($product_action === "edit") {
